cf21422f65a53b8cccef94e8588b3a9c17af74f6
Dylan Kirby <dylan@mojotech.com>
2017-02-20 11:10:11 -0500

    wip"

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.post('/webhook', function (req, res) {
   /* const myHeaders = new Headers(); */
   /* myHeaders.append("X-AUTH", process.env.xauth); */
   res.send({
-    "speech": "From South Attleboro you can leave at 3:55, 4:29, 5:55, or 6:35. Later you can leave South Station at 8:35, 9:45, 10:55, or 11:55.",
-    "displayText": "From South Attleboro you can leave at 3:55, 4:29, 5:55, or 6:35. Later you can leave South Station at 8:35, 9:45, 10:55, or 11:55.",
+    "speech": "From South Attleboro you can leave at 4:29, 5:55, or 6:35. Later you can leave South Station at 9:45, 10:55, or 11:55.",
+    "displayText": "From South Attleboro you can leave at 4:29, 5:55, or 6:35. Later you can leave South Station at 9:45, 10:55, or 11:55.",
     "data": {},
     "contextOut": [],
     "source": "MBTA"
